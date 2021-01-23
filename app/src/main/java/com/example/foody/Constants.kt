c9525fb8a8c046570f03c840ed58d6015567fdd5
yash <yashkumar201301@gmail.com>
2021-01-23 22:41:30 +0530

    Retrofit Setup

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "2de9a8cc395c464283d3ba0763a859d2"
+
+    }
+
+}
