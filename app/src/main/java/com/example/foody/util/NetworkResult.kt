170b72ef535e344b5fcf09e3ce822d5074a638ae
yash <yashkumar201301@gmail.com>
2021-01-29 14:02:38 +0530

    Display Recipe Fragment

--- app/src/main/java/com/example/foody/util/NetworkResult.kt
+++ app/src/main/java/com/example/foody/util/NetworkResult.kt
@@ -1,7 +1,7 @@
 package com.example.foody.util
 
 sealed class NetworkResult<T>(
-    val date: T? = null,
+    val data: T? = null,
     val message: String? = null
 ) {
     class Success<T>(data: T) : NetworkResult<T>(data)
