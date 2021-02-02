a9009033705111f649243d2c44acedca36b08aac
yash <yashkumar201301@gmail.com>
2021-02-02 16:17:34 +0530

    Overview Fragment

--- app/src/main/java/com/example/foody/models/Result.kt
+++ app/src/main/java/com/example/foody/models/Result.kt
@@ -1,7 +1,11 @@
 package com.example.foody.models
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
+@Parcelize
 data class Result(
 
     @SerializedName("aggregateLikes")
@@ -11,7 +15,7 @@ data class Result(
     @SerializedName("dairyFree")
     val dairyFree: Boolean,
     @SerializedName("extendedIngredients")
-    val extendedIngredients: List<ExtendedIngredient>,
+    val extendedIngredients: @RawValue List<ExtendedIngredient>,
     @SerializedName("glutenFree")
     val glutenFree: Boolean,
     @SerializedName("id")
@@ -35,4 +39,4 @@ data class Result(
     @SerializedName("veryHealthy")
     val veryHealthy: Boolean
 
-)
+) : Parcelable
