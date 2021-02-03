8080d4d3fa439d84526ec42bc7069c38d07cc59f
yash <yashkumar201301@gmail.com>
2021-02-03 13:10:11 +0530

    Ingredients

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -5,7 +5,10 @@ class Constants {
     companion object {
 
         const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
         const val API_KEY = "f483362e1d6242278e1ece66ba2040a2"
+
+        const val RECIPE_RESULT_KEY = "recipeBundle"
 
         // API Query Keys
         const val QUERY_SEARCH = "query"
